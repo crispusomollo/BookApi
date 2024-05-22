acec3dff453226b4ec13a986807abba0e2a56e6d
crispusomollo <crispusomollo@gmail.com>
2024-05-22 19:15:02 +0300

    import the configs into the main app

--- api/app.py
+++ api/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 from markupsafe import escape
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
+#@app.route('/')
+#def hello():
+#    name = request.args.get("name", "World")
+#    return f'Hello, {escape(name)}!'
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
