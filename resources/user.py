bbee7d35e6cf33d65eb8d96da45efa27c00ddcba
crispusomollo <crispusomollo@gmail.com>
2024-05-23 08:01:27 +0300

    Defined the user resources

---
+++ resources/user.py
@@ -0,0 +1,15 @@
+from flask import request
+from flask_restful import Resource
+from app import mongo
+
+class UserRegister(Resource):
+    def post(self):
+        data = request.get_json()
+        user_id = mongo.db.users.insert_one(data).inserted_id
+        return {"id": str(user_id)}, 201
+
+class UserProfile(Resource):
+    def get(self, user_id):
+        user = mongo.db.users.find_one_or_404({"_id": user_id})
+        return {"id": str(user["_id"]), "username": user["username"], "email": user["email"]}, 200
+
