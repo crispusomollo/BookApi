c5a83ae2444601f103acff47d92dc7f9ca1f96f3
crispusomollo <crispusomollo@gmail.com>
2024-05-23 08:20:51 +0300

    defined the read list resources

---
+++ resources/read_list.py
@@ -0,0 +1,15 @@
+from flask import request
+from flask_restful import Resource
+from api.app import mongo
+
+class ReadingList(Resource):
+    def get(self, user_id):
+        user = mongo.db.users.find_one_or_404({"_id": user_id})
+        return user.get("reading_list", []), 200
+
+class AddToReadingList(Resource):
+    def post(self, user_id, book_id):
+        user = mongo.db.users.find_one_or_404({"_id": user_id})
+        mongo.db.users.update_one({"_id": user_id}, {"$addToSet": {"reading_list": book_id}})
+        return {"msg": "Book added to reading list"}, 200
+
