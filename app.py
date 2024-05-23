d61eb1312b9418324ad053f06cb99df759c1db9a
crispusomollo <crispusomollo@gmail.com>
2024-05-23 09:35:16 +0300

    updated the main module

--- app.py
+++ app.py
@@ -18,17 +18,17 @@ api = Api(app)
 # Import resources and add routes
 from resources.book import BookList, BookDetail
 from resources.user import UserRegister, UserProfile
-from resources.reading_list import ReadingList, AddToReadingList
-from resources.reading_progress import ReadingProgress, UpdateReadingProgress
+#from resources.reading_list import ReadingList, AddToReadingList
+#from resources.reading_progress import ReadingProgress, UpdateReadingProgress
 
 api.add_resource(BookList, '/books')
 api.add_resource(BookDetail, '/books/<book_id>')
 api.add_resource(UserRegister, '/users')
 api.add_resource(UserProfile, '/users/<user_id>')
-api.add_resource(ReadingList, '/users/<user_id>/reading-list')
-api.add_resource(AddToReadingList, '/users/<user_id>/reading-list/<book_id>')
-api.add_resource(ReadingProgress, '/users/<user_id>/reading-progress/<book_id>')
-api.add_resource(UpdateReadingProgress, '/users/<user_id>/reading-progress/<book_id>')
+#api.add_resource(ReadingList, '/users/<user_id>/reading-list')
+#api.add_resource(AddToReadingList, '/users/<user_id>/reading-list/<book_id>')
+#api.add_resource(ReadingProgress, '/users/<user_id>/reading-progress/<book_id>')
+#api.add_resource(UpdateReadingProgress, '/users/<user_id>/reading-progress/<book_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
