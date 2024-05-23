9f40f0129eee068c27f012a253cdf708a5612593
crispusomollo <crispusomollo@gmail.com>
2024-05-23 14:03:20 +0300

    created v1 directory

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
-from views import app_views
+from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
-    """ GET /api/users
+    """ GET /api/v1/users
     Return:
       - list of all User objects JSON represented
     """
