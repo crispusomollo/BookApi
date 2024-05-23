819b475fb9757490af33ab6980e086af956dba4b
crispusomollo <crispusomollo@gmail.com>
2024-05-23 07:33:28 +0300

    Defined the book model

---
+++ models/book.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, title, author, isbn, cover_image, description, publisher, published_date, genre, pages):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.cover_image = cover_image
+        self.description = description
+        self.publisher = publisher
+        self.published_date = published_date
+        self.genre = genre
+        self.pages = pages
+
