cfbdea72f666d99b1e62f5fd0146b6de74d4f347
crispusomollo <crispusomollo@gmail.com>
2024-05-23 09:03:32 +0300

    updated the book resources

--- resources/book.py
+++ resources/book.py
@@ -1,20 +1,21 @@
-from flask import request
 from flask_restful import Resource
-from app import mongo
+from flask import request
+from bson.objectid import ObjectId
 
 class BookList(Resource):
     def get(self):
-        # This method will handle GET requests to the /books endpoint
+        from app import mongo
         books = mongo.db.books.find()
         return [{"id": str(book["_id"]), "title": book["title"], "author": book["author"]} for book in books], 200
 
     def post(self):
-        # This method will handle POST requests to the /books endpoint
+        from app import mongo
         data = request.get_json()
         book_id = mongo.db.books.insert_one(data).inserted_id
         return {"id": str(book_id)}, 201
 
 class BookDetail(Resource):
     def get(self, book_id):
-        book = mongo.db.books.find_one_or_404({"_id": book_id})
+        from app import mongo
+        book = mongo.db.books.find_one_or_404({"_id": ObjectId(book_id)})
         return {"id": str(book["_id"]), "title": book["title"], "author": book["author"], "isbn": book["isbn"]}, 200
