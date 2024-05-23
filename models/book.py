2a6b833064fb367f33cf4007be0dd37eaafce36e
crispusomollo <crispusomollo@gmail.com>
2024-05-23 10:23:41 +0300

    update the book model

--- models/book.py
+++ models/book.py
@@ -1,3 +1,4 @@
+from app import mongo
 class Book:
     def __init__(self, title, author, isbn, cover_image, description, publisher, published_date, genre, pages):
         self.title = title
@@ -9,4 +10,21 @@ class Book:
         self.published_date = published_date
         self.genre = genre
         self.pages = pages
+    
+    def save(self):
+        mongo.db.books.insert_one({
+            'title': self.title,
+            'author': self.author,
+            'isbn': self.isbn,
+            'cover_image': self.cover_image
+            'description': self.description,
+            'publisher': self.publisher,
+            'published_date': self.published_date,
+            'genre': self.genre,
+            'pages': self.pages
+        })
+
+    @staticmethod
+    def get_all():
+        return mongo.db.books.find({}, {'_id': False})
 
