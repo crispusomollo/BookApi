9ce50ca7f075efe2ca57685d126836658f612a5e
crispusomollo <crispusomollo@gmail.com>
2024-05-23 09:14:46 +0300

    refactored the user resources

--- resources/user.py
+++ resources/user.py
@@ -1,15 +1,17 @@
-from flask import request
+# resources/user.py
 from flask_restful import Resource
-from app import mongo
+from flask import request
 
 class UserRegister(Resource):
     def post(self):
+        from app import mongo
         data = request.get_json()
         user_id = mongo.db.users.insert_one(data).inserted_id
         return {"id": str(user_id)}, 201
 
 class UserProfile(Resource):
     def get(self, user_id):
-        user = mongo.db.users.find_one_or_404({"_id": user_id})
-        return {"id": str(user["_id"]), "username": user["username"], "email": user["email"]}, 200
-
+        from app import mongo
+        from bson.objectid import ObjectId
+        user = mongo.db.users.find_one_or_404({"_id": ObjectId(user_id)})
+        return {"id": str(user["_id"]), "name": user["name"], "email": user["email"]}, 200
